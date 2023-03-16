a04a4da7531d39cf5e43ad2168e9f80a06e5aad8
zeryx <1892175+zeryx@users.noreply.github.com>
2023-03-16 12:07:59 -0400

    added a participantStateChange function, restructured proposal checks to try/catch

--- actions/listen.ts
+++ actions/listen.ts
@@ -15,9 +15,20 @@ import {notifyDiscord} from "./discord";
 export const listenForProposalFn: ActionFn = async (context: Context, event: Event) => {
 	const txEvent = event as TransactionEvent;
 	let iface = new ethers.utils.Interface(Weavr.abi);
-	const result = iface.decodeEventLog("Proposal",
-		txEvent.logs[0].data, txEvent.logs[0].topics)
-	var {id, proposalType, creator, superMajority, info} = result;
+	let result = null;
+	for (let i = 0; i < txEvent.logs.length; i++) {
+		try {
+			result = iface.decodeEventLog("Proposal",
+				txEvent.logs[i].data, txEvent.logs[i].topics)
+		} catch (e) {
+			console.log("This event is not a Proposal", e)
+		}
+	}
+	if(result === null) {
+		console.log("Did not find proposal event, exiting")
+		return
+	}
+	let {id, proposalType, creator, superMajority, info} = result;
 	id = id.toString()
 	proposalType = proposalType.toString()
 	let current_proposals: Array<Object>;
@@ -33,12 +44,23 @@ export const listenForProposalFn: ActionFn = async (context: Context, event: Eve
 	await notifyDiscord(`New Proposal ${id} has been detected! https://www.weavr.org/#/dao/0x43240c0f5dedb375afd28206e02110e8fed8cFc0/proposal/${id}`, context)
 }
 
-export const updateOnStateChangeFn: ActionFn = async (context: Context, event: Event) => {
+export const UpdateOnProposalStateChangeFn: ActionFn = async (context: Context, event: Event) => {
 	const txEvent = event as TransactionEvent;
 	let iface = new ethers.utils.Interface(Weavr.abi);
-	const result = iface.decodeEventLog("ProposalStateChange",
-		txEvent.logs[0].data, txEvent.logs[0].topics)
-	var {id, state} = result;
+	let result = null;
+	for (let i = 0; i < txEvent.logs.length; i++) {
+		try {
+			result = iface.decodeEventLog("ProposalStateChange",
+				txEvent.logs[i].data, txEvent.logs[i].topics)
+		} catch (e) {
+			console.log("This event is not a ProposalStateChange", e)
+		}
+	}
+	if(result === null) {
+		console.log("Did not find ProposalStateChange event, exiting")
+		return
+	}
+	let {id, state} = result;
 	id = id.toString()
 	let STATES_ENUM = [
 		"Null",
@@ -60,3 +82,35 @@ export const updateOnStateChangeFn: ActionFn = async (context: Context, event: E
 	await notifyDiscord(`Proposal ${id} state is now ${state}, https://www.weavr.org/#/dao/0x43240c0f5dedb375afd28206e02110e8fed8cFc0/proposal/${id}`, context)
 }
 
+export const UpdateOnParticipantStateChangeFn: ActionFn = async (context: Context, event: Event) => {
+	const txEvent = event as TransactionEvent;
+	let iface = new ethers.utils.Interface(Weavr.abi);
+	let result = null;
+	for (let i = 0; i < txEvent.logs.length; i++) {
+		try {
+			result = iface.decodeEventLog("ParticipantChange",
+				txEvent.logs[i].data, txEvent.logs[i].topics)
+			console.log("Found ParticipantChange event")
+
+		} catch (e) {
+			console.log("This event is not a ParticipantChange", e)
+		}
+	}
+	if(result === null) {
+		console.log("Did not find ParticipantChange event, exiting")
+		return
+	}
+	let {participantType, participant } = result;
+	let PARTICIPANT_TYPE_ENUM = [
+		"Null",
+		"Removed",
+		"Genesis",
+		"KYC",
+		"Governor",
+		"Voucher",
+		"Individual Member",
+		"Corporate Member"
+		]
+	const state = PARTICIPANT_TYPE_ENUM[participantType]
+	await notifyDiscord(`participant ${participant} state is now ${state}`, context)
+}
